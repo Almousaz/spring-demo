37a0974a3b10ebd0af377eacb5c149edc10c8c33
Ali Mousazada <mousazada.ali@gmail.com>
2025-10-22 22:39:36 -0400

    Add integration test for TaskController

---
+++ src/test/java/com/example/demo/TaskControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+
+import com.example.demo.model.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testCreateAndFetchTasks() throws Exception {
+        Task task = new Task("Learn CI/CD");
+
+        mockMvc.perform(post("/tasks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(task)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Learn CI/CD"));
+    }
+}
